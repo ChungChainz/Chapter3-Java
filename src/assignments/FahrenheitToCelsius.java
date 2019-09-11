f9a362498c62f0024c070094d520ada268593ab5
Chris Molis <chrismolis12@gmail.com>
2019-09-11 09:11:59 -0500

    F to C push

--- src/assignments/FahrenheitToCelsius.java
+++ src/assignments/FahrenheitToCelsius.java
@@ -5,16 +5,52 @@ import java.util.Scanner;
 public class FahrenheitToCelsius {
     public static void main(String[] args)
     {
-        double morningTemp;
-        double noonTemp;
-        double eveningTemp;
+        double temp;
+        int i = 1;
 
-        Scanner inputDevice = new Scanner(System.in);
-        System.out.print("Enter the 8am temperature in F here >>> ");
-        morningTemp = inputDevice.nextDouble();
+        while (i < 4)
+        {
+            if(i == 1)
+            {
+                Scanner inputDevice = new Scanner(System.in);
+                System.out.print("Enter the 8 am temperature in F here >>> ");
+                temp = inputDevice.nextDouble();
 
-        Scanner inputDevice2 = new Scanner(System.in);
-        System.out.print("Enter the 12pm temperature in F here >>> ");
-        noonTemp = inputDevice2.nextDouble();
+                displayConversion(temp);
+                i++;
+            }
+            else if (i == 2)
+            {
+                Scanner inputDevice = new Scanner(System.in);
+                System.out.print("Enter the noon temperature in F here >>> ");
+                temp = inputDevice.nextDouble();
+
+                displayConversion(temp);
+                i++;
+            }
+            else
+            {
+                Scanner inputDevice = new Scanner(System.in);
+                System.out.print("Enter the 5 pm temperature in F here >>> ");
+                temp = inputDevice.nextDouble();
+
+                displayConversion(temp);
+                i++;
+            }
+        }
+    }
+    public static void displayConversion(double temp)
+    {
+        double celsius = (temp - 32) * 5/9;
+
+        if (celsius <= 0) {
+                System.out.println("It is " + temp + " degrees F. " + "\nIt is " + celsius + " degrees C." +
+                        " \nIt was FREEZING!");
+            } else if (celsius >= 37.7778) {
+                System.out.println("It is " + temp + " degrees F. " + "\nIt is " + celsius + " degrees C." +
+                        " \nIt was VERY HOT!");
+            } else {
+                System.out.println("It is " + temp + " degrees F. " + "\nIt is " + celsius + " degrees C.");
+            }
     }
 }
